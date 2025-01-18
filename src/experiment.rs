8c0acfe27f22c5a4b0a1897d7aa58bc4cfd60fd9
Toby Green <ybotybot01@gmail.com>
2025-01-18 19:17:38 +0000

    updating code

--- src/experiment.rs
+++ src/experiment.rs
@@ -9,8 +9,24 @@ enum ConcurrentMapInternal<const N: usize, V>{
 
 impl<const N: usize, V: Copy> ConcurrentMap<N, V>{
 
+    fn get_index(key: [u8; N], depth: usize) -> usize{
+        ((key[depth / 8] >> (depth % 8)) & 1) as usize
+    }
+    
     pub fn new() -> Self{
         Self(from_fn(|_| RwLock::new(Box::new(ConcurrentMapInternal::Item(None)))))
+    }
+
+    fn new_filled_list(item_1: ([u8; N], V), item_2: ([u8; N], V), depth: usize) -> ConcurrentMapInternal<N, V>{
+        ConcurrentMapInternal::List(
+            Self(from_fn(|x| {
+                RwLock::new(Box::new(ConcurrentMapInternal::Item({
+                    if Self::get_index(item_1.0, depth) == x {Some(item_1)}
+                    else if Self::get_index(item_2.0, depth) == x {Some(item_2)}
+                    else {None}
+                })))
+            }))
+        )
     }
 
     pub fn clear(&self){
@@ -24,20 +40,68 @@ impl<const N: usize, V: Copy> ConcurrentMap<N, V>{
     }
     
     fn get_internal(&self, key: [u8; N], depth: usize) -> Option<V>{
-        let modulus = depth % 8;
-        let byte = depth / 8;
-        
-        
-        self.0[index as usize].read().map(|read_lock| {
+        self.0[Self::get_index(key, depth)].read().map(|read_lock| {
             match read_lock.as_ref(){
-                ConcurrentMapInternal::Item(item) => {
-                    match item{
-                        None => None,
-                        Some(x) => if x.0 == key {Some(x.1)} else {None}
-                    }
-                }
+                ConcurrentMapInternal::Item(item) => return item.filter(|x| x.0 == key).map(|x| x.1),
                 ConcurrentMapInternal::List(list) => list.get_internal(key, depth + 1)
             }
         }).unwrap()
+    }
+
+    pub fn insert_or_update(&self, key: [u8; N], value: V) -> bool{
+    self.insert_or_update_if(key, value, &|_,_| true)
+    }
+    
+    pub fn insert_or_update_if(&self, key: [u8; N], value: V, should_update: &impl Fn(&V, &V) -> bool) -> bool{
+        self.insert_or_update_if_internal(key, value, should_update, 0)
+    }
+
+    fn insert_or_update_if_internal(&self, key: [u8; N], value: V, should_update: &impl Fn(&V, &V) -> bool, depth: usize) -> bool{
+        let index = Self::get_index(key, depth);
+        loop{
+            match self.0[index].read().map(|read_lock| {
+                match read_lock.as_ref(){
+                    ConcurrentMapInternal::Item(_) => None, //change to write lock
+                    ConcurrentMapInternal::List(list) => Some(list.insert_or_update_if_internal(key, value, should_update, depth + 1))
+                }
+            }).unwrap(){
+                None => (),
+                Some(x) => return x
+            }
+            match self.0[index].write().map(|mut write_lock| {
+                match write_lock.as_mut(){
+                    ConcurrentMapInternal::Item(item) => {
+                        //insert or update
+                        match item{
+                            Some(existing) => {
+                                if existing.0 == key{
+                                    Some(
+                                        if should_update(&existing.1, &value){
+                                            existing.1 = value;
+                                            true
+                                        }
+                                        else {false}
+                                    )
+                                }
+                                else{
+                                    //keep going down to next bit until find where the bits are not equal
+                                    
+                                    Some(true)
+                                }
+                            }
+                            None => {
+                                *item = Some((key, value));
+                                Some(true)
+                            }
+                        }
+                    }
+                    ConcurrentMapInternal::List(_) => None
+                }
+            }).unwrap(){
+                None => (),
+                Some(x) => return x
+            }
+        }
+        
     }
 }
