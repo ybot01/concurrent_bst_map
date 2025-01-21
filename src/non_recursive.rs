acacf8730ecc906eefa107e1c9ed9c30d7f98155
Toby Green <ybotybot01@gmail.com>
2025-01-21 17:56:53 +0000

    updating code

--- src/non_recursive.rs
+++ src/non_recursive.rs
@@ -6,6 +6,8 @@ fn get_index<const N: usize>(key: [u8; N], max_index: usize) -> usize{
     //need to get ratio of the key to max value of the key and then multiply that by max_index
 }
 
+const MIN_LIST_LENGTH: usize = 1024;
+
 #[derive(Debug)]
 pub struct ConcurrentMap<const N: usize, V>{
     inner: LazyLock<RwLock<ConcurrentMapInternal<N, V>>>
@@ -23,7 +25,7 @@ impl<const N: usize, V: Copy> ConcurrentMapInternal<N, V>{
             no_elements: AtomicUsize::new(0),
             list: {
                 let mut new_vec = Vec::new();
-                for _ in 0..1024 {new_vec.push(RwLock::new(Vec::new()))}
+                for _ in 0..MIN_LIST_LENGTH {new_vec.push(RwLock::new(Vec::new()))}
                 new_vec
             }
         }
@@ -32,6 +34,8 @@ impl<const N: usize, V: Copy> ConcurrentMapInternal<N, V>{
 
 impl<const N: usize, V: Copy> ConcurrentMap<N, V>{
 
+    
+    
     pub fn clear(&self){
         *self.inner.write().unwrap() = ConcurrentMapInternal::new();
     }
@@ -131,13 +135,38 @@ impl<const N: usize, V: Copy> ConcurrentMap<N, V>{
     }
     
     pub fn remove_if(&self, key: [u8; N], should_remove: impl Fn(&V) -> bool){
-        self.inner.read().map(|read_lock| {
+        if self.inner.read().map(|read_lock| {
             read_lock.list[get_index(key, read_lock.list.len())].write().map(|mut write_lock| {
                 match write_lock.iter().position(|x| x.0 == key){
-                    Some(index) => if should_remove(&write_lock[index].1) {_ = write_lock.swap_remove(index)},
-                    None => ()
+                    Some(index) => {
+                        if should_remove(&write_lock[index].1) {
+                            write_lock.swap_remove(index);
+                            (read_lock.no_elements.fetch_sub(1, Ordering::Relaxed) < (read_lock.list.len() / 2)) && (read_lock.list.len() > MIN_LIST_LENGTH)
+                        }
+                        else {false}
+                    }
+                    None => false
+                }
+            }).unwrap()
+        }).unwrap(){
+            self.inner.write().map(|mut write_lock| {
+                let old_list_length = write_lock.list.len();
+                let no_elements = write_lock.no_elements.load(Ordering::Relaxed);
+                let mut new_list_length = old_list_length;
+                while (new_list_length > MIN_LIST_LENGTH) && (no_elements < (new_list_length / 2)){new_list_length /= 2; }
+                if new_list_length > old_list_length{
+                    let mut new_vec = Vec::new();
+                    for _ in 0..new_list_length {new_vec.push(RwLock::new(Vec::<([u8; N], V)>::new()))}
+                    for i in 0..old_list_length{
+                        write_lock.list[i].read().map(|read_lock| {
+                            for entry in &*read_lock{
+                                new_vec[get_index(entry.0, new_list_length)].write().unwrap().push(*entry);
+                            }
+                        }).unwrap();
+                    }
+                    write_lock.list = new_vec;
                 }
             }).unwrap();
-        }).unwrap();
+        }
     }
 }
