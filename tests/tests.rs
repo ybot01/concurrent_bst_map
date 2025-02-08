a20671d618a5a940bb75ebaa1f56d7c70b6ba7c9
Toby Green <ybotybot01@gmail.com>
2025-02-08 17:07:06 +0000

    updating code

--- tests/tests.rs
+++ tests/tests.rs
@@ -4,6 +4,8 @@ use std::time::{Duration, SystemTime};
 use rand::{random};
 use rand::distr::{Distribution, StandardUniform};
 use tokio::task::JoinHandle;
+use rand::random_range;
+use rust_map::InsertOrUpdateResult;
 
 fn should_update<T: Ord>(value_1: &T, value_2: &T) -> bool{
     value_2 > value_1
@@ -15,89 +17,88 @@ fn get_vec_of_key_values<T>(length: usize) -> Vec<T> where StandardUniform: Dist
     to_return
 }
 
-mod limited_depth_tests{
-    use rand::random_range;
-    use concurrent_map::ConcurrentMap;
+mod concurrent_tests{
+    use rust_map::concurrent::Map;
     use super::*;
 
     #[test]
     fn length_test(){
         let expected = 10000;
-        let bst = ConcurrentMap::<32, u64>::new();
+        let map = Map::<32, u64>::new();
         get_vec_of_key_values::<([u8; 32],u64)>(expected).iter()
-            .for_each(|x| _ = bst.insert_or_update(x.0, x.1));
-        assert_eq!(bst.len(), expected);
+            .for_each(|x| _ = map.insert_or_update(x.0, x.1));
+        assert_eq!(map.len(), expected);
     }
 
     #[test]
     fn get_size(){
-        assert_eq!(size_of::<ConcurrentMap<32, u64>>(), 24);
+        assert_eq!(size_of::<Map<32, u64>>(), 24);
     }
 
     #[test]
     fn depth_test(){
-        let bst = ConcurrentMap::<32, u64>::new();
+        let map = Map::<32, u64>::new();
         let mut array = [0;32];
-        bst.insert_or_update(array,0);
+        map.insert_or_update(array,0);
         array[array.len()-1] = 1;
-        bst.insert_or_update(array, 0);
-        println!("{}", bst.depth());
+        map.insert_or_update(array, 0);
+        println!("{}", map.depth());
     }
     
     #[test]
     fn remove_test(){
         let expected = 10000;
         let to_insert = get_vec_of_key_values::<([u8; 32],u64)>(expected);
-        let bst = ConcurrentMap::<32, u64>::new();
-        to_insert.iter().for_each(|x| _ = bst.insert_or_update(x.0, x.1));
-        to_insert.iter().for_each(|x| bst.remove(x.0));
-        assert!(to_insert.iter().all(|x| bst.get(x.0).is_none()));
+        let map = Map::<32, u64>::new();
+        to_insert.iter().for_each(|x| _ = map.insert_or_update(x.0, x.1));
+        to_insert.iter().for_each(|x| map.remove(x.0));
+        assert!(to_insert.iter().all(|x| map.get(x.0).is_none()));
     }
 
     #[test]
     fn should_update_test() {
-        let bst = ConcurrentMap::<32, u64>::new();
+        let map = Map::<32, u64>::new();
         let (key, mut value) = ([0; 32], 0);
-        assert!(bst.insert_or_update_if(key, value, &should_update));
+        assert_eq!(map.insert_or_update_if(key, value, &should_update), InsertOrUpdateResult::Inserted);
         value += 1;
-        assert!(bst.insert_or_update_if(key, value, &should_update));
+        assert_eq!(map.insert_or_update_if(key, value, &should_update), InsertOrUpdateResult::Updated);
         value -= 1;
-        assert!(!bst.insert_or_update_if(key, value, &should_update));
+        assert_eq!(map.insert_or_update_if(key, value, &should_update), InsertOrUpdateResult::Neither);
     }
 
     #[test]
     fn insert_and_get_test() {
-        let bst = ConcurrentMap::<32, u64>::new();
-        _ = bst.insert_or_update([0; 32], 1);
-        assert!(bst.get([0;32]).is_some_and(|x| x == 1));
+        let map = Map::<32, u64>::new();
+        _ = map.insert_or_update([0; 32], 1);
+        assert!(map.get([0;32]).is_some_and(|x| x == 1));
     }
 
     #[test]
     fn get_closest_test() {
-        let bst = ConcurrentMap::<32, u64>::new();
-        _ = bst.insert_or_update([255; 32], 1);
-        _ = bst.insert_or_update([254; 32], 1);
-        //_ = bst.insert_or_update([0; 32], 1);
-        _ = bst.insert_or_update([1; 32], 1);
-        assert!(bst.get_or_closest_by_key([0;32], true, true).is_some_and(|x| x.0 == [255;32]));
+        let map = Map::<32, u64>::new();
+        _ = map.insert_or_update([255; 32], 1);
+        _ = map.insert_or_update([254; 32], 1);
+        //_ = map.insert_or_update([0; 32], 1);
+        _ = map.insert_or_update([1; 32], 1);
+        assert!(map.get_or_closest_by_key([0;32], true, true).is_some_and(|x| x.0 == [255;32]));
     }
 
     #[test]
     fn bench_insert_or_update_if(){
-        let bst = ConcurrentMap::<32, u64>::new();
+        let map = Map::<32, u64>::new();
         let (key, mut value) = ([0; 32], 0);
         let mut true_count = 0;
         let total = 1000000;
         let start_time = SystemTime::now();
         for _ in 0..total{
-            if bst.insert_or_update_if(key, value, &should_update) {true_count += 1};
+            if map.insert_or_update_if(key, value, &should_update) != InsertOrUpdateResult::Neither {true_count += 1};
             value += 1;
         }
         println!("{}", total as f64 / SystemTime::now().duration_since(start_time).unwrap().as_secs_f64());
         assert_eq!(true_count, total);
     }
 
-    static GLOBAL_BST: ConcurrentMap<32, [u8;32]> = ConcurrentMap::new();
+    static GLOBAL_MAP: Map<32, [u8;32]> = Map::new();
 
     static TRUE_COUNT: AtomicUsize = AtomicUsize::new(0);
 
@@ -123,7 +124,7 @@ mod limited_depth_tests{
                         USER_LIST.read().map(|read_lock| {
                             for i in start_index..(start_index+TOTAL_PER_THREAD) {
                                 let (key, value) = read_lock[i];
-                                if GLOBAL_BST.insert_or_update(key, value){
+                                if GLOBAL_MAP.insert_or_update(key, value) != InsertOrUpdateResult::Neither{
                                     TRUE_COUNT.fetch_add(1, Ordering::Relaxed);
                                 }
                             }
@@ -142,7 +143,7 @@ mod limited_depth_tests{
                 }
                 println!("{}", ((*NO_THREADS)*TOTAL_PER_THREAD) as f64 / max_duration.as_secs_f64());
                 assert_eq!(TRUE_COUNT.load(Ordering::Relaxed), (*NO_THREADS)*TOTAL_PER_THREAD);
-                println!("{} %", GLOBAL_BST.get_used_percent()*100.0);
+                println!("{} %", GLOBAL_MAP.get_used_percent()*100.0);
 
                 threads = Vec::new();
                 let rand_key = USER_LIST.read().unwrap()[random_range(0..USER_LIST.read().unwrap().len())].0;
@@ -150,7 +151,7 @@ mod limited_depth_tests{
                     threads.push(tokio::spawn(async move{
                         let start_time = SystemTime::now();
                         for _ in 0..TOTAL_PER_THREAD{
-                            _ = GLOBAL_BST.get_or_closest_by_key(rand_key, false, true);
+                            _ = GLOBAL_MAP.get_or_closest_by_key(rand_key, false, true);
                         }
                         SystemTime::now().duration_since(start_time).unwrap()
                     }))
@@ -172,7 +173,7 @@ mod limited_depth_tests{
                         let start_time = SystemTime::now();
                         USER_LIST.read().map(|read_lock| {
                             for i in start_index..(start_index+TOTAL_PER_THREAD) {
-                                GLOBAL_BST.remove(read_lock[i].0);
+                                GLOBAL_MAP.remove(read_lock[i].0);
                             }
                         }).unwrap();
                         SystemTime::now().duration_since(start_time).unwrap()
@@ -187,164 +188,111 @@ mod limited_depth_tests{
                     }
                 }
                 println!("{}", ((*NO_THREADS)*TOTAL_PER_THREAD) as f64 / max_duration.as_secs_f64());
-                assert_eq!(GLOBAL_BST.len(), 0);
+                assert_eq!(GLOBAL_MAP.len(), 0);
             });
     }
 }
 
-/*mod recursion_tests{
-    use concurrent_map::{ALWAYS_UPDATE, NEVER_UPDATE};
-    use concurrent_map::bst_map::ConcurrentBSTMap;
+mod non_concurrent_tests{
+    use rust_map::non_concurrent::Map;
     use super::*;
-    /*
-    #[test]
-    fn recursion_test(){
-        let mut key = [0; 32];
-        let bst = ConcurrentBSTMap::<32, u64>::new();
-        for i in 0..50000{
-            println!("{}", i);
-            _ = bst.insert_or_update(key, 0, &NEVER_UPDATE, 1000000);
-            for i in (0..32).rev(){
-                if key[i] == u8::MAX {key[i] = 0}
-                else {
-                    key[i] += 1;
-                    break
-                }
-            }
-        }
-    }*/
-    
+
     #[test]
     fn length_test(){
         let expected = 10000;
-        let bst = ConcurrentBSTMap::<32, u64, 256>::new();
+        let mut map = Map::<32, u64>::new();
         get_vec_of_key_values::<([u8; 32],u64)>(expected).iter()
-            .for_each(|x| _ = bst.insert_or_update(x.0, x.1, &ALWAYS_UPDATE));
-        assert_eq!(bst.len(), expected);
+            .for_each(|x| _ = map.insert_or_update(x.0, x.1));
+        assert_eq!(map.len(), expected);
+    }
+
+    #[test]
+    fn get_size(){
+        assert_eq!(size_of::<Map<32, u64>>(), 16);
     }
 
     #[test]
     fn depth_test(){
-        let expected = 10000;
-        let bst = ConcurrentBSTMap::<32, u64, 256>::new();
-        get_vec_of_key_values::<([u8; 32],u64)>(expected).iter()
-            .for_each(|x| _ = bst.insert_or_update(x.0, x.1, &ALWAYS_UPDATE));
-        println!("{}", bst.depth());
+        let mut map = Map::<32, u64>::new();
+        let mut array = [0;32];
+        map.insert_or_update(array,0);
+        array[array.len()-1] = 1;
+        map.insert_or_update(array, 0);
+        println!("max_depth: {}", map.depth());
     }
-
 
     #[test]
     fn remove_test(){
         let expected = 10000;
         let to_insert = get_vec_of_key_values::<([u8; 32],u64)>(expected);
-        let bst = ConcurrentBSTMap::<32, u64, 256>::new();
-        to_insert.iter().for_each(|x| _ = bst.insert_or_update(x.0, x.1, &ALWAYS_UPDATE));
-        to_insert.iter().for_each(|x| bst.remove(x.0));
-        assert!(to_insert.iter().all(|x| bst.get(x.0).is_none()));
+        let mut map = Map::<32, u64>::new();
+        to_insert.iter().for_each(|x| _ = map.insert_or_update(x.0, x.1));
+        to_insert.iter().for_each(|x| map.remove(x.0));
+        assert!(to_insert.iter().all(|x| map.get(x.0).is_none()));
     }
 
     #[test]
     fn should_update_test() {
-        let bst = ConcurrentBSTMap::<32, u64, 256>::new();
+        let mut map = Map::<32, u64>::new();
         let (key, mut value) = ([0; 32], 0);
-        assert!(bst.insert_or_update(key, value, &should_update).is_ok_and(|x| x));
+        assert_eq!(map.insert_or_update_if(key, value, &should_update), InsertOrUpdateResult::Inserted);
         value += 1;
-        assert!(bst.insert_or_update(key, value, &should_update).is_ok_and(|x| x));
+        assert_eq!(map.insert_or_update_if(key, value, &should_update), InsertOrUpdateResult::Updated);
         value -= 1;
-        assert!(!bst.insert_or_update(key, value, &should_update).is_ok_and(|x| x));
+        assert_eq!(map.insert_or_update_if(key, value, &should_update), InsertOrUpdateResult::Neither);
     }
 
     #[test]
     fn insert_and_get_test() {
-        let bst = ConcurrentBSTMap::<32, u64, 256>::new();
-        _ = bst.insert_or_update([0; 32], 1, &ALWAYS_UPDATE);
-        assert!(bst.get([0;32]).is_some_and(|x| x == 1));
+        let mut map = Map::<32, u64>::new();
+        _ = map.insert_or_update([0; 32], 1);
+        assert!(map.get([0;32]).is_some_and(|x| x == 1));
     }
 
     #[test]
-    fn bench_insert_or_update_if(){
-        let bst = ConcurrentBSTMap::<32, u64, 256>::new();
-        let (key, mut value) = ([0; 32], 0);
+    fn get_closest_test() {
+        let mut map = Map::<32, u64>::new();
+        _ = map.insert_or_update([255; 32], 1);
+        _ = map.insert_or_update([254; 32], 1);
+        //_ = map.insert_or_update([0; 32], 1);
+        _ = map.insert_or_update([1; 32], 1);
+        assert!(map.get_or_closest_by_key([0;32], true, true).is_some_and(|x| x.0 == [255;32]));
+    }
+
+    #[test]
+    fn bench(){
+        let mut map = Map::<32, u64>::new();
+        let mut key = [0; 32];
         let mut true_count = 0;
         let total = 1000000;
-        let start_time = SystemTime::now();
+        let mut start_time = SystemTime::now();
         for _ in 0..total{
-            if bst.insert_or_update(key, value, &should_update).is_ok_and(|x| x) {true_count += 1};
-            value += 1;
+            if map.insert_or_update(key, 0) != InsertOrUpdateResult::Neither {true_count += 1};
+            for i in (0..key.len()).rev(){
+                if key[i] == u8::MAX {key[i] = 0}
+                else {
+                    key[i] += 1;
+                    break;
+                }
+            }
         }
-        println!("{}", total as f64 / SystemTime::now().duration_since(start_time).unwrap().as_secs_f64());
+        println!("inserts/sec: {}", total as f64 / SystemTime::now().duration_since(start_time).unwrap().as_secs_f64());
         assert_eq!(true_count, total);
+        println!("used percent: {} %", map.get_used_percent()*100.0);
+        key = [0; 32];
+        start_time = SystemTime::now();
+        for _ in 0..total{
+            map.remove(key);
+            for i in (0..key.len()).rev(){
+                if key[i] == u8::MAX {key[i] = 0}
+                else {
+                    key[i] += 1;
+                    break;
+                }
+            }
+        }
+        println!("removes/sec: {}", total as f64 / SystemTime::now().duration_since(start_time).unwrap().as_secs_f64());
+        assert_eq!(map.len(), 0);
     }
 
-    static GLOBAL_BST: ConcurrentBSTMap<32, u64, 256> = ConcurrentBSTMap::new();
-
-    static TRUE_COUNT: AtomicUsize = AtomicUsize::new(0);
-
-    const NO_THREADS: usize = 10;
-    const TOTAL_PER_THREAD: usize = 100000;
-
-    static USER_LIST: LazyLock<RwLock<Vec<([u8; 32], u64)>>> = LazyLock::new(|| RwLock::new(get_vec_of_key_values(NO_THREADS*TOTAL_PER_THREAD)));
-
-    #[test]
-    fn bench_multi_thread_insert_or_update_if_and_remove(){
-        tokio::runtime::Builder::new_multi_thread()
-            .enable_all()
-            .build()
-            .unwrap()
-            .block_on(async {
-                _ = USER_LIST.read().unwrap().clone();
-                let mut threads= Vec::<JoinHandle<Duration>>::new();
-                for i in 0..NO_THREADS{
-                    threads.push(tokio::spawn(async move{
-                        let start_index = TOTAL_PER_THREAD * i;
-                        let start_time = SystemTime::now();
-                        USER_LIST.read().map(|read_lock| {
-                            for i in start_index..(start_index+TOTAL_PER_THREAD) {
-                                let (key, value) = read_lock[i];
-                                if GLOBAL_BST.insert_or_update(key, value, &NEVER_UPDATE).is_ok_and(|x| x){
-                                    TRUE_COUNT.fetch_add(1, Ordering::Relaxed);
-                                }
-                            }
-                        }).unwrap();
-                        SystemTime::now().duration_since(start_time).unwrap()
-                    }))
-                }
-                while threads.iter().any(|x| !x.is_finished()) {}
-                let mut max_duration = Duration::ZERO;
-                let mut duration;
-                for i in threads{
-                    duration = i.await.unwrap();
-                    if duration > max_duration{
-                        max_duration = duration;
-                    }
-                }
-                println!("{}", (NO_THREADS*TOTAL_PER_THREAD) as f64 / max_duration.as_secs_f64());
-                assert_eq!(TRUE_COUNT.load(Ordering::Relaxed), NO_THREADS*TOTAL_PER_THREAD);
-
-                threads = Vec::new();
-                for i in 0..NO_THREADS{
-                    threads.push(tokio::spawn(async move{
-                        let start_index = TOTAL_PER_THREAD * i;
-                        let start_time = SystemTime::now();
-                        USER_LIST.read().map(|read_lock| {
-                            for i in start_index..(start_index+TOTAL_PER_THREAD) {
-                                GLOBAL_BST.remove(read_lock[i].0);
-                            }
-                        }).unwrap();
-                        SystemTime::now().duration_since(start_time).unwrap()
-                    }))
-                }
-                while threads.iter().any(|x| !x.is_finished()) {}
-                max_duration = Duration::ZERO;
-                for i in threads{
-                    duration = i.await.unwrap();
-                    if duration > max_duration{
-                        max_duration = duration;
-                    }
-                }
-                println!("{}", (NO_THREADS*TOTAL_PER_THREAD) as f64 / max_duration.as_secs_f64());
-                assert_eq!(GLOBAL_BST.len(), 0);
-            });
-    }
-}*/
-
+}
